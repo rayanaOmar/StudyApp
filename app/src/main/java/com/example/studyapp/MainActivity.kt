e60d711d5486ec1d04af43d40bbbba3955006a86
rayanaOmar <rayanaomar_@hotmail.com>
2021-10-08 21:25:35 +0300

    StudyApp

---
+++ app/src/main/java/com/example/studyapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.studyapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btAndroid = findViewById<Button>(R.id.androidBt)
+
+        btAndroid.setOnClickListener {
+            val intent = Intent(this, AndroidActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btKotlin = findViewById<Button>(R.id.kotlinBt)
+
+        btKotlin.setOnClickListener {
+            val intent = Intent(this, KotlinActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
